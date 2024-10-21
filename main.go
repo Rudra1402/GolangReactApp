6612a5c96a1dff378447301956ef6142e569d31a
Rudra Patel <rudra.190410116095@gmail.com>
2024-10-21 00:21:50 -0400

    '/' route + struct definition

--- main.go
+++ main.go
@@ -7,8 +7,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type TODO struct {
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Go + React Application")
 	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"Message": "Server is running!"})
+	})
+
 	log.Fatal(app.Listen(":8001"))
 }
