a6362aaad5d11fc63295e7e00546f62aef076db1
Rudra Patel <rudra.190410116095@gmail.com>
2024-10-21 12:55:04 -0400

    CRUD APIs implemented

--- main.go
+++ main.go
@@ -17,9 +17,71 @@ func main() {
 	fmt.Println("Go + React Application")
 	app := fiber.New()
 
+	todos := []TODO{}
+
+	// Homepage
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"Message": "Server is running!"})
 	})
 
-	log.Fatal(app.Listen(":8001"))
+	// Get all TODOs
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	// Create a TODO
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &TODO{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "Body is required!"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+	// Update a TODO
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return err
+		}
+
+		for _, todo := range todos {
+			if todo.ID == id {
+				todo.Completed = true
+				return c.Status(201).JSON(todo)
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "TODO not found!"})
+	})
+
+	// Delete a TODO
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return err
+		}
+
+		for index, todo := range todos {
+			if todo.ID == id {
+				todos = append(todos[:index], todos[index+1:]...)
+				return c.Status(201).JSON(fiber.Map{"Message": "Todo deleted!"})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{"error": "TODO not found!"})
+	})
+
+	log.Fatal(app.Listen(":8081"))
 }
